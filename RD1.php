d0276091c0aec073c3de755d374d194226da7ebf
minghua <ming-hua@mail.chungyo.net>
2020-09-10 10:10:20 +0800

    update RD1.php

--- RD1.php
+++ RD1.php
@@ -138,17 +138,6 @@
                     url:"https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0002-001?Authorization=CWB-77A32A88-7BED-4DC6-805A-E3A2CBEBA227&format=JSON",
                     type:"GET",
                     success:function(jsonurl_rain){
-                        $.ajax({
-                            url:"rainDB.php",
-                            type:"post",
-                            data:{"jsonurl_rain":jsonurl_rain},
-                            success:function(){
-                                document.location.href="rainDB.php";
-                            },
-                            error:function(){
-                                alert("Error");
-                            }
-                        });
                         var location=jsonurl_rain["records"]["location"];
                         var weather_test=location[0]["weatherElement"];
                         var parameter_test=location[0]["parameter"];
